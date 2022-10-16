5ab1967eaf620ab13ccf632aec1fb8d2e70258e5
dinko <dinkoproto2@gmail.com>
2022-10-16 17:12:11 +0300

    Test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace excellentGrade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("opaa");
+        }
+    }
+}
