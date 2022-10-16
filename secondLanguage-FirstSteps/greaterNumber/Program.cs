a4335c80101e129457188d5d9135bee28f2e7dad
dinko <dinkoproto2@gmail.com>
2022-10-16 17:25:21 +0300

    Adding new tested tasks

---
+++ secondLanguage-FirstSteps/greaterNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace greaterNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            if(firstNum > secondNum)
+            {
+                Console.WriteLine(firstNum);
+            }
+            else
+            {
+                Console.WriteLine(secondNum);
+            }
+        }
+    }
+}
