20fef13fc8264b773b4db7e71547e9357566ad5f
dinko <dinkoproto2@gmail.com>
2022-10-16 18:12:21 +0300

    Adding new tasks

---
+++ secondLanguage-FirstSteps/sumSeconds/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace sumSeconds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstTime = int.Parse(Console.ReadLine());
+            int secondTimee = int.Parse(Console.ReadLine());
+            int thirdTime = int.Parse(Console.ReadLine());
+
+            int totalTime = firstTime + secondTimee + thirdTime;
+
+            int minutes = totalTime / 60;
+            int seconds = totalTime % 60;
+
+            if(seconds < 10)
+            {
+                Console.WriteLine($"{minutes}:0{seconds}");
+            }
+            else
+            {
+                Console.WriteLine($"{minutes}:{seconds}");
+            }
+        }
+    }
+}
