13322ce11f74f5226f4161cbe6b850fc4ed5e46b
dinko <dinkoproto2@gmail.com>
2022-10-16 17:56:50 +0300

    Adding new easy tasks

---
+++ secondLanguage-FirstSteps/areaOfFigures/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace areaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double figureOne = double.Parse(Console.ReadLine());
+                double area = figureOne * figureOne;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "rectangle")
+            {
+                double figureTwo = double.Parse(Console.ReadLine());
+                double figure2 = double.Parse(Console.ReadLine());
+                double area = figure2 * figureTwo;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "circle")
+            {
+                double figureThree = double.Parse(Console.ReadLine());
+                double area = Math.PI * figureThree * figureThree;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if (figure == "triangle")
+            {
+                double figureFour = double.Parse(Console.ReadLine());
+                double figure4 = double.Parse(Console.ReadLine());
+                double area = figure4 * figureFour / 2;
+                Console.WriteLine($"{area:F3}");
+            }
+        }
+    }
+}
---
+++ secondLanguage-FirstSteps/speedInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace speedInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num = double.Parse(Console.ReadLine());
+            if(num <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if(num <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if(num <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if(num <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
