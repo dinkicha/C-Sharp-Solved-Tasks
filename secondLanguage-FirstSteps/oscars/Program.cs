af65ee3bbf7c533ba58593e50e2a09299f4be95e
dinko <dinkoproto2@gmail.com>
2022-10-16 18:19:22 +0300

    Adding easy tasks for practising the new language

---
+++ secondLanguage-FirstSteps/oscars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oscars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double rent = double.Parse(Console.ReadLine());
+
+            double priceForStatues = rent * 0.70;
+            double priceForFood = priceForStatues * 0.85;
+            double priceForPlayback = priceForFood / 2;
+
+            double totalSum = rent + priceForStatues + priceForFood + priceForPlayback;
+
+            Console.WriteLine($"{totalSum:f2}");
+        }
+    }
+}
